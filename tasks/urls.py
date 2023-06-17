536e35ba197f31125a88fa6297a0ba14d6896110
enriquers <enriquers.dev@gmail.com>
2023-06-16 18:21:39 -0600

    Rn we can create, delete, update and complete tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,7 @@ from tasks import views
 urlpatterns = [
     path('', views.list_tasks),
     path('new/', views.create_task, name='new_task' ),
-    path('delete/<int:task_id>', views.delete_task, name='delete_task' )
+    path('delete/<int:task_id>', views.delete_task, name='delete_task'),
+    path('complete/<int:task_id>', views.complete_task, name='complete_task'),
+    path('update/<int:task_id>', views.update_task, name='update_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,16 @@ from tasks.models import Task
 
 # Create your views here.
 def list_tasks(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(completed=False)
+    completed_tasks_list = []
+    completed_tasks = Task.objects.filter(completed=True)
+    
+    for task in range(5):
+        completed_tasks_list.append(completed_tasks[(len(completed_tasks)-1)-task])
+    
     return render(request, 'list_tasks.html', {
-        "tasks": tasks
+        "tasks": tasks,
+        "completed_tasks": completed_tasks_list
     })
 
 def create_task(request):
@@ -21,3 +28,28 @@ def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     return redirect('/tasks/')
+
+def complete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('/tasks/')
+
+def update_task(request, task_id):
+    if request.method == 'GET':
+        task = Task.objects.filter(id=task_id)
+        return render(request, 'update_task.html',{
+            'id': task[0].id,
+            'title': task[0].title,
+            'description': task[0].description
+        })
+    else:
+        new_title = request.POST['title']
+        new_description = request.POST['description']
+        
+        task = Task.objects.get(id=task_id)
+        task.title = new_title
+        task.description = new_description
+        task.save()
+        
+        return redirect('/tasks/')
